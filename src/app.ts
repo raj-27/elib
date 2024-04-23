b568043c52d28676b6a3a679ef78b2001b2d6d41
Raj <raj819314@gmail.com>
2024-04-23 21:40:05 +0530

    Add book model

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,14 @@
-import express, { Request, Response } from "express";
-import createHttpError from "http-errors";
+import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorhandler";
 import userRouter from "./user/userRouter";
+import bookRouter from "./book/bookRouter";
 
 const app = express();
 
 app.use(express.json());
 
-// Routes
-// HTTP methods: GET,POST,PATCH,DELETE
-app.get("/", (req: Request, res: Response, next) => {
-  // const error = createHttpError(400, "Something went wrong");
-  // throw error;
-  res.json({
-    message: "Welcome to elib apis",
-  });
-});
-
 app.use("/api/user", userRouter);
+app.use("/api/book", bookRouter);
 
 // Global Error Handler
 app.use(globalErrorHandler);
