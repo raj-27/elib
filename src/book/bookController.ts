036dd24f24d3a2dbc14a3d8a64297395a5dff790
Raj <raj819314@gmail.com>
2024-04-24 15:44:34 +0530

    upload file to cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,8 +1,52 @@
 import { NextFunction, Request, Response } from "express";
+import cloudinary from "../config/cloudinary";
+import path from "path";
+import createHttpError from "http-errors";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   const {} = req.body;
-  res.json(req.files);
+  try {
+    const files = req.files as { [filename: string]: Express.Multer.File[] };
+    const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+    const filename = files.coverImage[0].filename;
+    const filepath = path.resolve(
+      __dirname,
+      "../../public/data/uploads",
+      filename
+    );
+    const uploadResult = await cloudinary.uploader.upload(filepath, {
+      filename_override: filename,
+      folder: "book-cover",
+      format: coverImageMimeType,
+    });
+
+    const bookFileName = files.file[0].filename;
+    const bookFilePath = path.resolve(
+      __dirname,
+      "../../public/data/uploads",
+      bookFileName
+    );
+
+    const bookFileUploadResult = await cloudinary.uploader.upload(
+      bookFilePath,
+      {
+        resource_type: "raw",
+        filename_override: bookFileName,
+        folder: "book-pdfs",
+        format: "pdf",
+      }
+    );
+    console.log({ bookFileUploadResult });
+    console.log({ uploadResult });
+
+    res.json(req.files);
+  } catch (err) {
+    const error = createHttpError(
+      500,
+      "Error while uploading file to cloudinary"
+    );
+    return next(error);
+  }
 };
 
 export { createBook };
